854d7f8a8afd3bb48db61d6234c38f18d9cdafb4
gayathrid2304 <gayathridh23@gmail.com>
2024-07-06 20:15:02 +0530

    This is my second commit

--- First_Program.java
+++ First_Program.java
@@ -1,5 +1,6 @@
 public class First_Program{
     public static void main(String[] args) {
         System.out.println("This is my first program");
+        System.out.println("Extra statement");
     }
 }
